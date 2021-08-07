b7de195b83dc5028d362811d243ad7798ec53ef7
10n <ionut.cioflan@gmail.com>
2021-08-07 14:37:00 +0300

    Update BoundingBoxInterface.php

--- src/AnthonyMartin/GeoLocation/Interfaces/BoundingBoxInterface.php
+++ src/AnthonyMartin/GeoLocation/Interfaces/BoundingBoxInterface.php
@@ -8,7 +8,7 @@ use AnthonyMartin\GeoLocation;
 interface BoundingBoxInterface {
   public function __construct($geopoint, $distance, $unit_of_measurement);
   public function setGeoPoint(GeoPoint $geopoint);
-  public function setUnit($unit)
+  public function setUnit($unit);
   public function setDistance($distance);
   public function calculate();
 
